5e9cbd8bb4bc31e5e408bd3a84ca51eed6906aae
mrypdm <36487113+mrypdm@users.noreply.github.com>
2023-06-21 16:21:30 +0300

    fix comment

--- examples/ShareLibUsageExample/TestCppSharedLib/CoolStringBuilder.h
+++ examples/ShareLibUsageExample/TestCppSharedLib/CoolStringBuilder.h
@@ -7,7 +7,7 @@ private:
     std::stringstream _builder;
 
 public:
-	// In `Extern "C"` we cannot use templates
+    // In `Extern "C"` we cannot use templates
     CoolStringBuilder *Append(const char* value);
 
     CoolStringBuilder *AppendLine(const char* value);
