b566bb35b7a39dead034acc98d3c9b510a2ab96c
Maxim Shapovalov <mrypdm@gmail.com>
2023-10-01 15:39:31 +0300

    remaining changes

---
+++ src/GUI/Exceptions/TabExistsException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using GUI.Controls;
+
+namespace GUI.Exceptions;
+
+/// <summary>
+/// The exception that is thrown when the tab already exists
+/// </summary>
+public class TabExistsException : InvalidOperationException
+{
+    public TabExistsException(string message = "Tab already exists", Exception innerException = null) :
+        base(message, innerException)
+    {
+    }
+    
+    /// <summary>
+    /// Reference to an existing tab
+    /// </summary>
+    public FileTab Tab { get; init; }
+}
---
+++ src/GUI/Managers/TabManager.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using Avalonia;
+using Avalonia.Media;
+using GUI.Controls;
+using GUI.Exceptions;
+using GUI.Models;
+
+namespace GUI.Managers;
+
+/// <summary>
+/// Tab manager
+/// </summary>
+public class TabManager
+{
+    private static readonly IBrush DefaultBackground = new SolidColorBrush(Colors.White);
+    private static readonly IBrush SelectedBackground = new SolidColorBrush(Colors.LightSkyBlue, 0.5D);
+
+    private readonly ICommand _defaultCommand;
+
+    /// <summary>
+    /// Creates new instance of tab manager
+    /// </summary>
+    /// <param name="defaultCommand">Default tab command</param>
+    public TabManager(ICommand defaultCommand)
+    {
+        _defaultCommand = defaultCommand;
+        SelectTab(CreateTab(new FileModel()));
+    }
+
+    /// <summary>
+    /// Current selected tab
+    /// </summary>
+    public FileTab CurrentTab { get; private set; }
+
+    /// <summary>
+    /// Collection of all tabs
+    /// </summary>
+    public ObservableCollection<FileTab> Tabs { get; } = new();
+
+    /// <summary>
+    /// Creates new tab for file and put it into <see cref="Tabs"/>
+    /// </summary>
+    /// <param name="file">File info</param>
+    /// <param name="command">Tab command (or default, if null)</param>
+    /// <returns>Created tab</returns>
+    /// <exception cref="TabExistsException">If tab for file already exists</exception>
+    public FileTab CreateTab(FileModel file, ICommand command = null)
+    {
+        var tab = Tabs.SingleOrDefault(t => t.Tag as FileModel == file);
+        if (tab != null)
+        {
+            throw new TabExistsException("Tab for that file already exists")
+            {
+                Tab = tab
+            };
+        }
+
+        tab = new FileTab(file)
+        {
+            Header = file.FileName,
+            Margin = new Thickness(5, 0, 5, 0),
+            Command = command ?? _defaultCommand
+        };
+        tab.CommandParameter = tab;
+
+        Tabs.Add(tab);
+
+        return tab;
+    }
+
+    /// <summary>
+    /// Deletes tab. If there are no tabs left, it creates an empty tab.
+    /// </summary>
+    /// <param name="tab">Tab reference</param>
+    public void DeleteTab(FileTab tab)
+    {
+        Tabs.Remove(tab);
+
+        if (!Tabs.Any())
+        {
+            SelectTab(CreateTab(new FileModel()));
+        }
+    }
+
+    /// <summary>
+    /// Changes current tab
+    /// </summary>
+    /// <param name="tab">Tab reference</param>
+    public void SelectTab(FileTab tab)
+    {
+        if (CurrentTab != null)
+        {
+            CurrentTab.Background = DefaultBackground;
+        }
+
+        CurrentTab = tab;
+        CurrentTab.Background = SelectedBackground;
+    }
+}
