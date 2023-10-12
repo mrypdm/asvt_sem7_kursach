eca8d18d8791b58537f8eb5e2b396ccc86ac317d
Masterluch <34518167+Masterluch@users.noreply.github.com>
2023-10-12 15:27:53 +0300

    Initial commit

---
+++ src/AssemblerApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using AssemblerLib;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string main_asm_file = "main.asm";
+        string[] linked_asm_files = new string[] { "macro.asm" };
+
+        Assembler asm = new(ref main_asm_file, ref linked_asm_files);
+    }
+}
---
+++ src/AssemblerLib/Assembler.cs
@@ -0,0 +1,15 @@
+﻿namespace AssemblerLib
+{
+    public class Assembler
+    {
+        public Assembler(ref string main_asm_file, ref string[] linked_asm_files)
+        {
+            //code
+        }
+
+        public void assemble()
+        {
+            //code
+        }
+    }
+}
