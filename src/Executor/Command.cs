3025c5adfd370ef2bd4d19d68bccd33790c68493
reichsstolz <styrkoffpost@gmail.com>
2023-10-23 01:45:05 +0300

    Add OpcodeIndentifyer

--- src/Executor/Command.cs
+++ src/Executor/Command.cs
@@ -2,6 +2,27 @@ using System;
 using Argument;
 
 namespace Executor{
+    public class OpcodeIndentifyer {
+
+        private OpcodeIndentifyer Instance;
+
+        private Dictionary<string, string> TwoOperands;
+        private Dictionary<string, string> OneOperand;
+        private Dictionary<string, string> BranchOperationC0;
+        private Dictionary<string, string> BranchOperationC1;
+        public OpcodeIndentifyer GetInstance(){
+            if (Instance == null){
+                Instance = new OpcodeIndentifyer()
+            }
+            return Instance
+        }
+        private OpcodeIndentifyer(){
+            
+        }
+        public GetCommandName(){
+            
+        }
+    }
 
     public record Argument{
         
@@ -10,11 +31,10 @@ namespace Executor{
         private Argument Arguments[2];
         public State Execute(State CurrentState);
 
+        public Command(Memory memory, ushort address){
+
+        }
+
     }
-
-    public int MatchArgumentsNumber(ushort opcode){
-
-    }
-
 
 }
--- src/Executor/Executor.cs
+++ src/Executor/Executor.cs
@@ -1,30 +1,66 @@
 using System;
 
 namespace Executor{
+
+    enum Flag{
+        Z,
+        N,
+        C, 
+        V, 
+        T
+    }
     
-    public record State {
+    public class State {
+        
+        private static State Instance;
         public ushort ProcessorStateWord; // flags
         public ushort MemoryAddressRegister; // РАП
         public ushort[8] R;
+
+        public State GetInstance(){
+            if (Instance == null){
+                Instance = new State();
+            }
+            return Instance;
+        }
+
+        public SetFlag(Flag flag, bool val)[
+
+        ]
+
+        private State(){
+            Array.Fill(R, 0);
+            ProcessorStateWord = 0;
+            MemoryAddressRegister = 0;
+        }
+
     }
 
     public static class Executor {
         
-        public Executor(State NewState){
-            this.CurrentState = NewState;
+        private static Executor Instance;
+
+        public Executor GetInstance(){
+            if (Instance == null){
+                Instance = new Executor(State.GetInstance())
+            }
+            
+        }
+        private Executor(State NewState){
+            CurrentState = NewState;
         }
         private State CurrentState;
         private Command CurrentCommand;
         public int ExecuteProgram();
 
         private int ReadCommand(){
-            this.CurrentCommand = Command(this.CurrentState.MemoryAddressRegister);
+            CurrentCommand = Command(CurrentState.MemoryAddressRegister);
             
         }
         public int ExecuteNextInstruction(){
-            this.CurrentState.MemoryAddressRegister = this.CurrentState.R[7];
-            this.CurrentState.R[7] += 2;
-            this.ReadCommand(this.CurrentState[7]);
+            CurrentState.MemoryAddressRegister = CurrentState.R[7];
+            CurrentState.R[7] += 2;
+            ReadCommand(CurrentState[7]);
 
         }
         public int LoadProgram(string filename){
