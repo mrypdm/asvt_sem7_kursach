51371ee94f7155d70c0c5e447667f42f060563ea
Maxim Shapovalov <mrypdm@gmail.com>
2023-11-03 11:13:59 +0300

    tests for PathHelper

---
+++ src/Tests/Shared.Tests/PathHelperTests.Unix.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Shared.Helpers;
+
+namespace Shared.Tests;
+
+/// <summary>
+/// Tests for <see cref="PathHelper"/> on <see cref="Platforms.Unix"/> platform
+/// </summary>
+[Platform(Platforms.Unix)]
+public class PathHelperUnixTests
+{
+    [Test]
+    [TestCaseSource(nameof(CombineTestUnixSource))]
+    public void CombineTestUnix(string path1, string path2, string expected)
+    {
+        Assert.That(PathHelper.Combine(path1, path2), Is.EqualTo(expected));
+    }
+
+    [Test]
+    [TestCaseSource(nameof(GetDirectoryNameTestUnixSource))]
+    public void GetDirectoryNameTestUnix(string path, string expected)
+    {
+        Assert.That(PathHelper.GetDirectoryName(path), Is.EqualTo(expected));
+    }
+
+    [Test]
+    [TestCaseSource(nameof(GetFileNameTestUnixSource))]
+    public void GetFileNameTest(string path, string expected)
+    {
+        Assert.That(PathHelper.GetFileName(path), Is.EqualTo(expected));
+    }
+
+    [Test]
+    [TestCaseSource(nameof(GetRelativePathTestUnixSource))]
+    public void GetRelativePathTestUnix(string relativeTo, string path, string expected)
+    {
+        Assert.That(PathHelper.GetRelativePath(relativeTo, path), Is.EqualTo(expected));
+    }
+
+    [Test]
+    [TestCaseSource(nameof(GetRelativePathInvalidThrowsTestSource))]
+    public void GetRelativePathInvalidThrowsTest(string relativeTo, string path)
+    {
+        Assert.Catch<ArgumentException>(() => PathHelper.GetRelativePath(relativeTo, path));
+    }
+
+    private static readonly object[] CombineTestUnixSource =
+    {
+        new[] { "/a", "b/c.txt", "/a/b/c.txt" },
+        new[] { "/a/..", "b/c.txt", "/b/c.txt" },
+        new[] { "/a/.", "b/c.txt", "/a/b/c.txt" },
+        new[] { "/a/./../a", "./b/c.txt", "/a/b/c.txt" },
+        new[] { "/a", "/b", "/b" }
+    };
+
+    private static readonly object[] GetDirectoryNameTestUnixSource =
+    {
+        new[] { "/a/b/c.txt", "/a/b" },
+        new[] { "/a/../b/c.txt", "/b" },
+        new[] { "/a/./b/c.txt", "/a/b" },
+        new[] { "/a/./../a/./b/c.txt", "/a/b" },
+        new[] { "/", null }
+    };
+
+    private static readonly object[] GetFileNameTestUnixSource =
+    {
+        new[] { "/a/b/c.txt", "c.txt" },
+        new[] { "/a/../b/c.txt", "c.txt" },
+        new[] { "/a/./b/c.txt", "c.txt" },
+        new[] { "/a/./../a/./b/c.txt", "c.txt" },
+        new[] { "/", "" },
+        new[] { "c.txt", "c.txt" },
+        new[] { "./c.txt", "c.txt" },
+        new[] { "../c.txt", "c.txt" },
+        new string[] { null, null }
+    };
+
+    private static readonly object[] GetRelativePathTestUnixSource =
+    {
+        new[] { "/a", "/c.txt", "../c.txt" },
+        new[] { "/a/..", "/c.txt", "c.txt" },
+        new[] { "/a/.", "/c.txt", "../c.txt" }
+    };
+
+    private static readonly object[] GetRelativePathInvalidThrowsTestSource =
+    {
+        new string[] { null, null },
+        new[] { "  ", "" },
+    };
+}
---
+++ src/Tests/Shared.Tests/PathHelperTests.Win.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Shared.Helpers;
+
+namespace Shared.Tests;
+
+/// <summary>
+/// Tests for <see cref="PathHelper"/> on <see cref="Platforms.Win"/> platform
+/// </summary>
+[Platform(Platforms.Win)]
+public class PathHelperWinTests
+{
+    [Test]
+    [TestCaseSource(nameof(CombineTestWinSource))]
+    public void CombineTestWin(string path1, string path2, string expected)
+    {
+        Assert.That(PathHelper.Combine(path1, path2), Is.EqualTo(expected));
+    }
+
+    [Test]
+    [TestCaseSource(nameof(GetDirectoryNameTestWinSource))]
+    public void GetDirectoryNameTestWin(string path, string expected)
+    {
+        Assert.That(PathHelper.GetDirectoryName(path), Is.EqualTo(expected));
+    }
+
+    [Test]
+    [TestCaseSource(nameof(GetFileNameTestWinSource))]
+    public void GetFileNameTest(string path, string expected)
+    {
+        Assert.That(PathHelper.GetFileName(path), Is.EqualTo(expected));
+    }
+
+    [Test]
+    [TestCaseSource(nameof(GetRelativePathTestWinSource))]
+    public void GetRelativePathTestWin(string relativeTo, string path, string expected)
+    {
+        Assert.That(PathHelper.GetRelativePath(relativeTo, path), Is.EqualTo(expected));
+    }
+
+    [Test]
+    [TestCaseSource(nameof(GetRelativePathInvalidThrowsTestSource))]
+    public void GetRelativePathInvalidThrowsTest(string relativeTo, string path)
+    {
+        Assert.Catch<ArgumentException>(() => PathHelper.GetRelativePath(relativeTo, path));
+    }
+
+    private static readonly object[] CombineTestWinSource =
+    {
+        new[] { "C:\\a", "b\\c.txt", "C:\\a\\b\\c.txt" },
+        new[] { "C:\\a\\..", "b\\c.txt", "C:\\b\\c.txt" },
+        new[] { "C:\\a\\.", "b\\c.txt", "C:\\a\\b\\c.txt" },
+        new[] { "C:\\a\\.\\..\\a", ".\\b\\c.txt", "C:\\a\\b\\c.txt" },
+        new[] { "C:\\a", "C:\\b", "C:\\b" }
+    };
+
+    private static readonly object[] GetDirectoryNameTestWinSource =
+    {
+        new[] { "C:\\a\\b\\c.txt", "C:\\a\\b" },
+        new[] { "C:\\a\\..\\b\\c.txt", "C:\\b" },
+        new[] { "C:\\a\\.\\b\\c.txt", "C:\\a\\b" },
+        new[] { "C:\\a\\.\\..\\a\\.\\b\\c.txt", "C:\\a\\b" },
+        new[] { "C:\\", null }
+    };
+
+    private static readonly object[] GetFileNameTestWinSource =
+    {
+        new[] { "C:\\a\\b\\c.txt", "c.txt" },
+        new[] { "C:\\a\\..\\b\\c.txt", "c.txt" },
+        new[] { "C:\\a\\.\\b\\c.txt", "c.txt" },
+        new[] { "C:\\a\\.\\..\\a\\.\\b\\c.txt", "c.txt" },
+        new[] { "C:\\", "" },
+        new[] { "c.txt", "c.txt" },
+        new[] { "./c.txt", "c.txt" },
+        new[] { "../c.txt", "c.txt" },
+        new string[] { null, null }
+    };
+
+    private static readonly object[] GetRelativePathTestWinSource =
+    {
+        new[] { "C:\\a", "C:\\c.txt", "..\\c.txt" },
+        new[] { "C:\\a\\..", "C:\\c.txt", "c.txt" },
+        new[] { "C:\\a\\.", "C:\\c.txt", "..\\c.txt" },
+        new[] { "C:\\", "D:\\folder", "D:\\folder" }
+    };
+
+    private static readonly object[] GetRelativePathInvalidThrowsTestSource =
+    {
+        new string[] { null, null },
+        new[] { "  ", "" },
+        new[] { "C:\\", " " },
+        new[] { " ", "C:\\c.txt" }
+    };
+}
