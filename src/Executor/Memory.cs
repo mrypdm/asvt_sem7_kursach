31b626db55c876e17cfa21b9c0d75f46ec79c59e
reichsstolz <styrkoffpost@gmail.com>
2023-11-09 00:13:40 +0300

    add memory

--- src/Executor/Memory.cs
+++ src/Executor/Memory.cs
@@ -1,10 +1,38 @@
 using System;
 
-namespace Executor
-{
-    public class Memory{
-        
-    
-    }
+namespace Executor;
 
+interface IMemory {
+  ushort GetWord(ushort address);
+  byte GetByte(ushort address);
+  void SetWord(ushort address, ushort value);
+  void SetByte(ushort address, byte value);
 }
+
+public class Memory: IMemory{
+        private ushort[] RawMemory;
+
+        public byte GetByte(ushort address){
+            if (address % 2 == 0){
+                return (byte)(RawMemory[address] >> 8);
+            }
+            return (byte)(RawMemory[address] & 255);
+        }
+
+        public ushort GetWord(ushort address) => RawMemory[address];
+
+        public void SetByte(ushort address, byte value){
+            RawMemory[address] = value;
+        }
+
+        public byte SetWord(ushort address, ushort value){
+            return;
+        }
+
+        Memory(){
+            RawMemory = new ushort[32513];
+        }
+               
+}
+
+
