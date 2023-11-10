c63dc7e82a2634f33849b2307e33458f885f29f0
Masterluch <34518167+Masterluch@users.noreply.github.com>
2023-11-11 00:54:03 +0300

    Added comment removing in parsing

--- src/AssemblerLib/Parser.cs
+++ src/AssemblerLib/Parser.cs
@@ -8,14 +8,17 @@ namespace AssemblerLib;
 
 internal class Parser
 {
-    private const string RegexPattern = @"^\s*([^\s,:]+:\s*)?(\S+)?\s*([^\s,]+\s*,?\s*){0,}$";
+    private const string _RegexPatternCommandLine = @"^\s*([^\s,:]+:\s*)?(\S+)?\s*([^\s,]+\s*,?\s*){0,}$";
+    private const string _RegexPatternRemovingComment = @"^[^;.]+(?=;?)";
     private readonly char[] BadSymbols = { ' ', '\t', ',', ':' };
 
-    private readonly Regex _regexMask;
-    
+    private readonly Regex _regexMaskCommandLine;
+    private readonly Regex _regexMaskRemovingComment;
+
     public Parser()
     {
-        _regexMask = new Regex(RegexPattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        _regexMaskCommandLine = new Regex(_RegexPatternCommandLine, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        _regexMaskRemovingComment = new Regex(_RegexPatternRemovingComment, RegexOptions.IgnoreCase | RegexOptions.Singleline);
     }
 
     public async Task<List<CommandLine>> Parse(string filePath)
@@ -27,7 +30,12 @@ internal class Parser
 
         while ((line = await reader.ReadLineAsync()) != null)
         {
-            var match = _regexMask.Match(line);
+            // Removing comment
+            line = _regexMaskRemovingComment.Match(line).Groups[0].Value;
+            if (line == "") { continue; }
+
+            // Splitting command line
+            var match = _regexMaskCommandLine.Match(line);
 
             var mark = match.Groups[1].Value.Trim().Trim(BadSymbols).ToLower();
             var instruction = match.Groups[2].Value.Trim(BadSymbols).ToLower();
