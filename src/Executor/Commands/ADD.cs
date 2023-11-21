1479fe80cefb4f1abab31fdce1cbeac750137df6
reichsstolz <styrkoffpost@gmail.com>
2023-11-21 23:06:00 +0300

    Add execute mov & add

--- src/Executor/Commands/ADD.cs
+++ src/Executor/Commands/ADD.cs
@@ -1,9 +1,9 @@
 namespace Executor;
 
-public class MOV: TwoOperands {
+public class ADD: TwoOperands {
     private Memory memory;
     private State state;
-    public MOV(State state, Memory memory): base(state, memory) {}
+    public ADD(State state, Memory memory): base(state, memory) {}
 
     public override IArgument[] GetArguments(ushort word) {
       IArgument[] args = new TwoOperandsArg[2];
@@ -12,11 +12,9 @@ public class MOV: TwoOperands {
       return args;
     }
     public override void Execute(IArgument[] arguments) {
-      var value = arguments[0].GetValue();
-      arguments[1].SetValue(value);
-      return;
+      
     }
 
-    public override ushort Opcode => 0b1_0000_0000_0000;
+    public override ushort Opcode => 0b110_0000_0000_0000;
 
 }
