627a4fdb09b95a69bc9f7e2a0cb9bc8774d1c16a
mrYPDM <mrypdm@gmail.com>
2023-12-04 18:21:38 +0300

    better validator

--- src/Devices/Validators/IDeviceValidator.cs
+++ src/Devices/Validators/IDeviceValidator.cs
@@ -9,6 +9,13 @@ public interface IDeviceValidator
     /// Validates device
     /// </summary>
     /// <param name="path">Path to device</param>
+    /// <param name="errorMessage">Fail reason</param>
     /// <returns>True if device is valid</returns>
-    bool Validate(string path);
+    bool Validate(string path, out string errorMessage);
+
+    /// <summary>
+    /// Throws if device is ivalid
+    /// </summary>
+    /// <param name="path">Path to device</param>
+    void ThrowIfInvalid(string path);
 }
