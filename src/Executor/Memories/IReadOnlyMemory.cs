ad40ed6ce045cb93a46e73a8b43c2efb49e31eec
Maxim Shapovalov <mrypdm@gmail.com>
2023-12-08 21:14:07 +0300

    fix

---
+++ src/Executor/Memories/IReadOnlyMemory.cs
@@ -0,0 +1,10 @@
+﻿namespace Executor.Memories;
+
+public interface IReadOnlyMemory
+{
+    IReadOnlyCollection<byte> GetMemory();
+
+    ushort GetWord(ushort address);
+
+    byte GetByte(ushort address);
+}
