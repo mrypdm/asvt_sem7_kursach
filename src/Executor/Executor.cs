9d594b1ac733f783e03415abba65adf0e55bbcc1
Maxim Shapovalov <mrypdm@gmail.com>
2023-12-09 00:14:56 +0300

    device support

--- src/Executor/Executor.cs
+++ src/Executor/Executor.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Threading.Tasks;
 using Devices.Managers;
 using Devices.Providers;
+using Devices.Validators;
 using Domain.Models;
 using Executor.States;
 using Executor.Storages;
@@ -14,9 +15,10 @@ public class Executor
 {
     private readonly IState _state;
     private readonly IStorage _memory;
+    private readonly IDeviceValidator _deviceValidator;
     private readonly IDevicesManager _devicesManager;
     private readonly IStorage _bus;
-    
+
     private readonly OpcodeIdentifier _opcodeIdentifier;
 
     public IReadOnlyStorage Memory => _memory;
@@ -24,16 +26,18 @@ public class Executor
     public ushort ProcessorStateWord => _state.ProcessorStateWord;
 
     public IReadOnlyCollection<ushort> Registers => _state.Registers;
-    
+
     public IProject Project { get; private set; }
-    
+
     public string BinaryFile { get; private set; }
 
     public Executor()
     {
         _state = new State();
         _memory = new Memory();
-        _devicesManager = new DevicesManager(new DeviceProvider());
+        var provider = new DeviceProvider();
+        _devicesManager = new DevicesManager(provider);
+        _deviceValidator = new DeviceValidator(provider);
         _bus = new Bus(_memory, _devicesManager);
         _opcodeIdentifier = new OpcodeIdentifier(_state, _bus);
     }
@@ -52,6 +56,12 @@ public class Executor
         return 0;
     }
 
+    private void AddDevice(string path)
+    {
+        _deviceValidator.ThrowIfInvalid(path);
+        _devicesManager.Add(path);
+    }
+
     public async Task Reload()
     {
         _state.Registers[6] = Project.StackAddress;
@@ -59,11 +69,35 @@ public class Executor
 
         using var reader = new StreamReader(BinaryFile);
 
+        // file format:
+        // 000000 - code section
+        // ...
+        // 000001' - relocatable address
+        // ...
+        // 000000
+        // #/path/to/device.dll - devices section
+
         var address = Project.ProgramAddress;
         while (await reader.ReadLineAsync() is { } line)
         {
-            var word = Convert.ToUInt16(line, 8);
-            _memory.SetWord(address, word);
+            if (line.StartsWith("#"))
+            {
+                AddDevice(line);
+                continue;
+            }
+
+            var isRelocatable = line.EndsWith('\'');
+
+            var word = isRelocatable
+                ? Convert.ToUInt16(line[..6], 8) + Project.ProgramAddress
+                : Convert.ToUInt16(line, 8);
+
+            if (word > ushort.MaxValue)
+            {
+                throw new OutOfMemoryException("Program is too large");
+            }
+
+            _memory.SetWord(address, (ushort)word);
             address += 2;
         }
     }
@@ -74,8 +108,7 @@ public class Executor
         {
             throw new InvalidOperationException("Start program address cannot be odd");
         }
-        
-        
+
         if (project.StackAddress % 2 == 1)
         {
             throw new InvalidOperationException("Start stack address cannot be odd");
