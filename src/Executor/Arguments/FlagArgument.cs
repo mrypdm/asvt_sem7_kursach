83a466d2e679b1669e62ed3bb54e987f383d50ad
Maxim Shapovalov <mrypdm@gmail.com>
2023-12-10 12:01:01 +0300

    flag command

---
+++ src/Executor/Arguments/FlagArgument.cs
@@ -0,0 +1,30 @@
+﻿using Executor.Arguments.Abstraction;
+using Executor.Exceptions;
+
+namespace Executor.Arguments;
+
+public class FlagArgument : IArgument
+{
+    public FlagArgument(ushort word)
+    {
+        C = (word & 1) != 0;
+        V = (word & 2) != 0;
+        Z = (word & 4) != 0;
+        N = (word & 8) != 0;
+        ToSet = (word & 16) != 0;
+    }
+
+    public object GetValue() => (ToSet, N, Z, V, C);
+
+    public void SetValue(object obj) => throw new ReadOnlyArgumentException(GetType());
+
+    public bool ToSet { get; }
+    
+    public bool C { get; }
+
+    public bool V { get; }
+
+    public bool Z { get; }
+
+    public bool N { get; }
+}
---
+++ src/Executor/Commands/MiscellaneousInstructions/FlagCommand.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Executor.Arguments;
+using Executor.Arguments.Abstraction;
+using Executor.CommandTypes;
+using Executor.Exceptions;
+using Executor.States;
+using Executor.Storages;
+
+namespace Executor.Commands.MiscellaneousInstructions;
+
+public class FlagCommand : BaseCommand
+{
+    public FlagCommand(IStorage storage, IState state) : base(storage, state)
+    {
+    }
+
+    public override void Execute(IArgument[] arguments)
+    {
+        if (arguments.Length != 1)
+        {
+            throw new ArgumentException("Count of arguments must be 1", nameof(arguments));
+        }
+        
+        if (arguments[0] is not FlagArgument)
+        {
+            throw new InvalidArgumentTypeException(new[] { typeof(FlagArgument) }, new[] { arguments[0].GetType() });
+        }
+
+        var validatedArgument = (FlagArgument)arguments[0];
+
+        if (validatedArgument.ToSet)
+        {
+            State.C = validatedArgument.C || State.C;
+            State.V = validatedArgument.V || State.V;
+            State.Z = validatedArgument.Z || State.Z;
+            State.N = validatedArgument.N || State.N;
+        }
+        else
+        {
+            State.C = !validatedArgument.C && State.C;
+            State.V = !validatedArgument.V && State.V;
+            State.Z = !validatedArgument.Z && State.Z;
+            State.N = !validatedArgument.N && State.N;
+        }
+    }
+
+    public override IArgument[] GetArguments(ushort word) => new IArgument[] { new FlagArgument(word) };
+
+    public override ushort Opcode => Convert.ToUInt16("000240", 8);
+}
