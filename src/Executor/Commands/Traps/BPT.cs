311595408a44b145c06c8ce1156726b4af95ff08
Maxim Shapovalov <mrypdm@gmail.com>
2023-12-10 10:11:21 +0300

    traps

---
+++ src/Executor/Commands/Traps/BPT.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Executor.Arguments.Abstraction;
+using Executor.CommandTypes;
+using Executor.States;
+using Executor.Storages;
+
+namespace Executor.Commands.Traps;
+
+public class BPT : TrapInstruction
+{
+    private const ushort InterruptVectorAddress = 12; // 0o14
+
+    public BPT(IStorage storage, IState state) : base(storage, state)
+    {
+    }
+
+    public override void Execute(IArgument[] arguments) => HandleTrap(InterruptVectorAddress);
+
+    public override IArgument[] GetArguments(ushort word) => Array.Empty<IArgument>();
+
+    public override ushort Opcode => Convert.ToUInt16("000003", 8);
+}
---
+++ src/Executor/Commands/Traps/TRAP.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Executor.Arguments.Abstraction;
+using Executor.CommandTypes;
+using Executor.States;
+using Executor.Storages;
+
+namespace Executor.Commands.Traps;
+
+public class TRAP : TrapInstruction
+{
+    private const ushort InterruptVectorAddress = 28; // 0o34
+
+    public TRAP(IStorage storage, IState state) : base(storage, state)
+    {
+    }
+
+    public override void Execute(IArgument[] arguments) => HandleTrap(InterruptVectorAddress);
+
+    public override IArgument[] GetArguments(ushort word) => Array.Empty<IArgument>();
+
+    public override ushort Opcode => Convert.ToUInt16("104400", 8);
+}
