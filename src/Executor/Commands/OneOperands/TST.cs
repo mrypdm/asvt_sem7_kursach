abe352f00161c2f73a91a7c09b0b2535589a248f
Maxim Shapovalov <mrypdm@gmail.com>
2023-12-12 11:34:40 +0300

    tst, tstb

---
+++ src/Executor/Commands/OneOperands/TST.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Executor.Arguments;
+using Executor.Arguments.Abstraction;
+using Executor.CommandTypes;
+using Executor.Extensions;
+using Executor.States;
+using Executor.Storages;
+
+namespace Executor.Commands.OneOperands;
+
+public class TST : OneOperand
+{
+    public TST(IStorage storage, IState state) : base(storage, state)
+    {
+    }
+
+    public override void Execute(IArgument[] arguments)
+    {
+        ValidateArgumentsCount(arguments, 1);
+        var validatedArgument = ValidateArgument<RegisterWordArgument>(arguments[0]);
+
+        var value = validatedArgument.Value;
+
+        validatedArgument.Value = value;
+        State.Z = value == 0;
+        State.N = value.IsNegative();
+        State.V = false;
+        State.C = false;
+    }
+
+    public override ushort Opcode => Convert.ToUInt16("005700", 8);
+}
---
+++ src/Executor/Commands/OneOperands/TSTB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Executor.Arguments;
+using Executor.Arguments.Abstraction;
+using Executor.CommandTypes;
+using Executor.Extensions;
+using Executor.States;
+using Executor.Storages;
+
+namespace Executor.Commands.OneOperands;
+
+public class TSTB : OneOperand
+{
+    public TSTB(IStorage storage, IState state) : base(storage, state)
+    {
+    }
+
+    public override void Execute(IArgument[] arguments)
+    {
+        ValidateArgumentsCount(arguments, 1);
+        var validatedArgument = ValidateArgument<RegisterByteArgument>(arguments[0]);
+
+        var value = validatedArgument.Value;
+
+        validatedArgument.Value = value;
+        State.Z = value == 0;
+        State.N = value.IsNegative();
+        State.V = false;
+        State.C = false;
+    }
+
+    public override ushort Opcode => Convert.ToUInt16("105700", 8);
+}
