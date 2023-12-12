76781d301bacc6d449a121a565f4894e2ac044cc
Maxim Shapovalov <mrypdm@gmail.com>
2023-12-12 17:07:05 +0300

    asl, aslb

---
+++ src/Executor/Commands/OneOperands/ASL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Executor.Arguments;
+using Executor.Arguments.Abstraction;
+using Executor.CommandTypes;
+using Executor.Extensions;
+using Executor.States;
+using Executor.Storages;
+
+namespace Executor.Commands.OneOperands;
+
+public sealed class ASL : OneOperand
+{
+    public ASL(IStorage storage, IState state) : base(storage, state)
+    {
+    }
+
+    /// <inheritdoc />
+    public override void Execute(IArgument[] arguments)
+    {
+        ValidateArgumentsCount(arguments, 1);
+        var validatedArgument = ValidateArgument<RegisterWordArgument>(arguments[0]);
+
+        var value = validatedArgument.Value;
+        var newC = value.IsNegative();
+
+        value <<= 1;
+
+        validatedArgument.Value = value;
+        State.Z = value == 0;
+        State.N = value.IsNegative();
+        State.C = newC;
+        State.V = State.N ^ State.C;
+    }
+
+    /// <inheritdoc />
+    public override ushort OperationCode => Convert.ToUInt16("006300", 8);
+}
