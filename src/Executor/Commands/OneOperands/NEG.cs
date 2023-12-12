a64b1ac021207dc7a03f1b68df9cef40b3f5cdac
Maxim Shapovalov <mrypdm@gmail.com>
2023-12-12 11:12:10 +0300

    neg, negb

---
+++ src/Executor/Commands/OneOperands/NEG.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Executor.Arguments;
+using Executor.Arguments.Abstraction;
+using Executor.CommandTypes;
+using Executor.Extensions;
+using Executor.States;
+using Executor.Storages;
+
+namespace Executor.Commands.OneOperands;
+
+public class NEG : OneOperand
+{
+    public NEG(IStorage storage, IState state) : base(storage, state)
+    {
+    }
+
+    public override void Execute(IArgument[] arguments)
+    {
+        ValidateArgumentsCount(arguments, 1);
+        var validatedArgument = ValidateArgument<RegisterWordArgument>(arguments[0]);
+
+        var value = (ushort)-validatedArgument.Value;
+
+        validatedArgument.Value = value;
+        State.Z = value == 0;
+        State.N = value.IsNegative();
+        State.V = value == 0x8000;
+        State.C = value != 0;
+    }
+
+    public override ushort Opcode => Convert.ToUInt16("005400", 8);
+}
---
+++ src/Executor/Commands/OneOperands/NEGB.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Executor.Arguments;
+using Executor.Arguments.Abstraction;
+using Executor.CommandTypes;
+using Executor.Extensions;
+using Executor.States;
+using Executor.Storages;
+
+namespace Executor.Commands.OneOperands;
+
+public class NEGB : OneOperand
+{
+    public NEGB(IStorage storage, IState state) : base(storage, state)
+    {
+    }
+
+    public override void Execute(IArgument[] arguments)
+    {
+        ValidateArgumentsCount(arguments, 1);
+        var validatedArgument = ValidateArgument<RegisterByteArgument>(arguments[0]);
+
+        var value = (byte)-validatedArgument.Value;
+
+        validatedArgument.Value = value;
+        State.Z = value == 0;
+        State.N = value.IsNegative();
+        State.V = value == 0x80;
+        State.C = value != 0;
+    }
+
+    public override ushort Opcode => Convert.ToUInt16("105400", 8);
+}
