b3d3f9f418f342239330a0e60c78c1b4b5c0062d
Maxim Shapovalov <mrypdm@gmail.com>
2023-12-12 11:32:26 +0300

    mfps, mtps

---
+++ src/Executor/Commands/OneOperands/MFPS.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Executor.Arguments;
+using Executor.Arguments.Abstraction;
+using Executor.CommandTypes;
+using Executor.Extensions;
+using Executor.States;
+using Executor.Storages;
+
+namespace Executor.Commands.OneOperands;
+
+public class MFPS : OneOperand
+{
+    public MFPS(IStorage storage, IState state) : base(storage, state)
+    {
+    }
+
+    public override void Execute(IArgument[] arguments)
+    {
+        ValidateArgumentsCount(arguments, 1);
+        var validatedArgument = ValidateArgument<RegisterByteArgument>(arguments[0]);
+
+        var value = (byte)State.ProcessorStateWord;
+
+        if (validatedArgument.Mode == 0)
+        {
+            // propagate the sign bit
+            var high = value.IsNegative() ? 0xFF : 0;
+            State.Registers[validatedArgument.Register] = (ushort)((high << 8) | value);
+        }
+        else
+        {
+            validatedArgument.Value = value;
+        }
+
+        State.Z = value == 0;
+        State.N = value.IsNegative();
+        State.V = false;
+    }
+
+    public override ushort Opcode => Convert.ToUInt16("106700", 8);
+}
---
+++ src/Executor/Commands/OneOperands/MTPS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Executor.Arguments;
+using Executor.Arguments.Abstraction;
+using Executor.CommandTypes;
+using Executor.States;
+using Executor.Storages;
+
+namespace Executor.Commands.OneOperands;
+
+public class MTPS : OneOperand
+{
+    public MTPS(IStorage storage, IState state) : base(storage, state)
+    {
+    }
+
+    public override void Execute(IArgument[] arguments)
+    {
+        ValidateArgumentsCount(arguments, 1);
+        var validatedArgument = ValidateArgument<RegisterByteArgument>(arguments[0]);
+
+        var value = validatedArgument.Value;
+
+        // this instruction cannot set the T bit, but it does not say about clearing
+        // for now we will completely prohibit changing the T bit
+        value &= 0b1110_1111; // clear T bit
+        value |= (byte)((State.T ? 1 : 0) << 4); // set original T
+
+        State.ProcessorStateWord = value;
+    }
+
+    public override ushort Opcode => Convert.ToUInt16("106400", 8);
+}
